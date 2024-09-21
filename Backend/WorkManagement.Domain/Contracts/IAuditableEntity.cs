ad7e6c082156f1f71badec4308762fb770edbda1
Inzamam Nawab <inawab@uplandsoftware.com>
2024-09-21 20:48:09 +0530

    Fixing Interface

--- Backend/WorkManagement.Domain/Contracts/IAuditableEntity.cs
+++ Backend/WorkManagement.Domain/Contracts/IAuditableEntity.cs
@@ -8,9 +8,9 @@ namespace WorkManagement.Domain.Contracts
 {
     public interface IAuditableEntity
     {
-        public int CreatedBy { get; set; }
+        public Guid CreatedBy { get; set; }
         public DateTimeOffset CreatedOn { get; set; }
-        public int? LastModifiedBy { get; set; }
+        public Guid? LastModifiedBy { get; set; }
         public DateTimeOffset? LastModifiedOn { get; set; }
     }
 }
