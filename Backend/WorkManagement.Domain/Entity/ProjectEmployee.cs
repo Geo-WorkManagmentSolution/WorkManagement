2fe0716e315c9336c5707a2fc840baf3fc93d6db
geo-nshah <shahnaupul0@gmail.com>
2024-10-15 15:57:29 -0400

    Backend: Add projectEmployee table

---
+++ Backend/WorkManagement.Domain/Entity/ProjectEmployee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using WorkManagement.Domain.Models;
+using WorkManagementSolution.Employee;
+
+namespace WorkManagement.Domain.Entity
+{
+    public class ProjectEmployee : FullyAuditableEntity
+    {
+        [ForeignKey(nameof(Employee))]
+        public int EmployeeId { get; set; }
+        public Employee? Employee { get; set; }
+
+        [ForeignKey(nameof(Project))]
+        public int ProjectId { get; set; }
+        public Project? Project { get; set; }
+    }
+}
