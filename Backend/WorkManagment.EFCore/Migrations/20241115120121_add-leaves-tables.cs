f0f17d1a857b891c5f5b4f5c3bca24b5876670fb
geo-nshah <shahnaupul0@gmail.com>
2024-11-15 07:02:16 -0500

    Backend: Add migration scripts

---
+++ Backend/WorkManagment.EFCore/Migrations/20241115120121_add-leaves-tables.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkManagement.EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class addleavestables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmployeeHolidays",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsFloater = table.Column<bool>(type: "bit", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeHolidays", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeLeaveType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsPaid = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeLeaveType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeDefaultLeave",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeLeaveTypeId = table.Column<int>(type: "int", nullable: true),
+                    TotalLeaves = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeDefaultLeave", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmployeeDefaultLeave_EmployeeLeaveType_EmployeeLeaveTypeId",
+                        column: x => x.EmployeeLeaveTypeId,
+                        principalTable: "EmployeeLeaveType",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeLeaves",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LeaveDays = table.Column<double>(type: "float", nullable: false),
+                    EmployeeLeaveTypeId = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeLeaves", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmployeeLeaves_EmployeeLeaveType_EmployeeLeaveTypeId",
+                        column: x => x.EmployeeLeaveTypeId,
+                        principalTable: "EmployeeLeaveType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmployeeLeaves_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeLeaveSummary",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    RemainingLeaves = table.Column<double>(type: "float", nullable: false),
+                    EmployeeLeaveTypeId = table.Column<int>(type: "int", nullable: true),
+                    TotalLeaves = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeLeaveSummary", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmployeeLeaveSummary_EmployeeLeaveType_EmployeeLeaveTypeId",
+                        column: x => x.EmployeeLeaveTypeId,
+                        principalTable: "EmployeeLeaveType",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_EmployeeLeaveSummary_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeDefaultLeave_EmployeeLeaveTypeId",
+                table: "EmployeeDefaultLeave",
+                column: "EmployeeLeaveTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeLeaves_EmployeeId",
+                table: "EmployeeLeaves",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeLeaves_EmployeeLeaveTypeId",
+                table: "EmployeeLeaves",
+                column: "EmployeeLeaveTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeLeaveSummary_EmployeeId",
+                table: "EmployeeLeaveSummary",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeLeaveSummary_EmployeeLeaveTypeId",
+                table: "EmployeeLeaveSummary",
+                column: "EmployeeLeaveTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeDefaultLeave");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeHolidays");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeLeaves");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeLeaveSummary");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeLeaveType");
+        }
+    }
+}
