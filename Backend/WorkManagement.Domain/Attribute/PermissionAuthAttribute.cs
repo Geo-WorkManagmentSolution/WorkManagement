cfbe4588732effe7b9cb7e37f4a0cb8553ca758a
Inzamam Nawab <inawab@uplandsoftware.com>
2024-12-16 19:05:23 +0530

    Adding Authpolicy provider

--- Backend/WorkManagement.Domain/Attribute/PermissionAuthAttribute.cs
+++ Backend/WorkManagement.Domain/Attribute/PermissionAuthAttribute.cs
@@ -20,38 +20,7 @@ namespace WorkManagement.Domain.Attribute
     }
 
 
-    public class PermissionAuthPolicyProvider : IAuthorizationPolicyProvider
-    {
-        const string POLICY_PREFIX = "Permission_";
-        bool AllowsCachingPolicies => true;
-
-        public Task<AuthorizationPolicy> GetDefaultPolicyAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<AuthorizationPolicy?> GetFallbackPolicyAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        // Policies are looked up by string name, so expect 'parameters' (like age)
-        // to be embedded in the policy names. This is abstracted away from developers
-        // by the more strongly-typed attributes derived from AuthorizeAttribute
-        // (like [MinimumAgeAuthorize()] in this sample)
-        public Task<AuthorizationPolicy> GetPolicyAsync(string policyName)
-        {
-            var permissionAction = policyName.Substring(POLICY_PREFIX.Length, policyName.Length);
-            if (policyName.StartsWith(POLICY_PREFIX, StringComparison.OrdinalIgnoreCase))
-            {
-                var policy = new AuthorizationPolicyBuilder();
-                policy.AddRequirements(new PermissionRequirement(permissionAction));
-                return Task.FromResult(policy.Build());
-            }
-
-            return Task.FromResult<AuthorizationPolicy>(null);
-        }
-    }
+   
 
 
 }
