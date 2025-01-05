70d27342a541309ad8dfe13f8d8eb6ca5185fb73
Inzamam Nawab <inawab@uplandsoftware.com>
2025-01-05 22:55:05 +0530

    merge with main

---
+++ Backend/WorkManagement.Api/wwwroot/assets/EmployeeApp-DRmOKMUY.js
@@ -0,0 +1,1 @@
+import{j as t,O as e}from"./index-Cf0SbAd0.js";function p(){return t(e,{})}export{p as default};
---
+++ Backend/WorkManagement.Api/wwwroot/assets/ProjectApp-CikBongk.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-Cf0SbAd0.js";function o(){return t(r,{})}export{o as default};
